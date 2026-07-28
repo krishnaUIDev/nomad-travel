514dba8958e5d412c5c8f7e03582b35c38e744d2
krishnaUIDev <krishna.kondoju007@gmail.com>
2026-07-27 22:22:35 -0400

    refactor: flatten Sources directory structure and remove redundant subfolder

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "NomadTravel",
-            path: "Sources/NomadTravel"
+            path: "Sources"
         )
     ]
 )
